11c4061f1c2158245274418da0f4b0a185ff4a7f
FoxerJuan <juan.foxer@gmail.com>
2020-05-04 17:59:44 -0300

    se agrego tp2 resuelto

---
+++ TP2/ConsoleAppTP2/Problema1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTP2
+{
+    public class Problema1
+    {
+        public double Calcular(double numero1, double numero2, string operador)
+        {
+            double resultado;
+            switch (operador)
+            {
+                case "-":
+                    resultado = numero1 - numero2;
+                    break;
+                case "/":
+                    resultado = numero1 / numero2;
+                    break;
+                case "*":
+                    resultado = numero1 * numero2;
+                    break;
+                case "+":
+                default:
+                    resultado = numero1 + numero2;
+                    break;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ TP2/ConsoleAppTP2/Problema2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleAppTP2
+{
+    public class Problema2
+    {
+        public string CalcularYMostrar(string operador1, string operador2, string operador, out decimal resultado)
+        {
+            string cadenaResultante;
+            try
+            {
+                decimal numero1 = Decimal.Parse(operador1, new CultureInfo("es-AR"));
+                decimal numero2 = Decimal.Parse(operador2, new CultureInfo("es-AR"));
+                switch (operador)
+                {
+                    case "-":
+                        resultado = numero1 - numero2;
+                        break;
+                    case "/":
+                        resultado = numero1 / numero2;
+                        break;
+                    case "*":
+                        resultado = numero1 * numero2;
+                        break;
+                    case "+":
+                        resultado = numero1 + numero2;
+                        break;
+                    default:
+                        throw new System.InvalidOperationException("");
+                }
+
+                cadenaResultante = $"{numero1}{operador}{numero2}={resultado}";
+                cadenaResultante = cadenaResultante.Replace(".", ",");
+            }
+            catch
+            {
+                resultado = 0;
+                cadenaResultante = "Operacion no valida";
+            }
+            
+            return cadenaResultante;
+        }
+    }
+}
